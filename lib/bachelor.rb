65c581a3e141b3052ec2d106e451234b833daff3
Simon Lee <simonlee0306@gmail.com>
2018-10-24 22:30:41 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,74 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+  name = ""
+  data.each do |num_season, array_of_contestants| #"season 30", "season 29" ..
+    if num_season == season
+      array_of_contestants.each do |data| #{:name => "xyz"}, {}, {} ..
+        data.each do |key, value|
+          if key == "status" && value == "Winner"
+            name = data["name"]
+            name_array = name.split(" ")
+            return name_array[0]
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+
+  data.each do |num_season, array_of_contestants| #"season 30", "season 29" ..
+    array_of_contestants.each do |data| #{:name => "xyz"}, {}, {} ..
+      data.each do |key, value|
+        if key == "occupation" and value == occupation
+          return data["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |num_season, array_of_contestants| #"season 30", "season 29" ..
+    array_of_contestants.each do |data| #{:name => "xyz"}, {}, {} ..
+      data.each do |key, value|
+        if key == "hometown" && value == hometown
+          count += 1
+        end
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |num_season, array_of_contestants| #"season 30", "season 29" ..
+    array_of_contestants.each do |data| #{:name => "xyz"}, {}, {} ..
+      data.each do |key, value|
+        if key == "hometown" && value == hometown
+          return data["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total = 0.00
+  count = 0.00
+  data.each do |num_season, array_of_contestants| #"season 30", "season 29" ..
+    if num_season == season
+      array_of_contestants.each do |data| #{:name => "xyz"}, {}, {} ..
+        data.each do |key, value|
+          if key == "age"
+            count += 1
+            total += data["age"].to_f
+          end
+        end
+      end
+    end
+  end
+  (total/count).round
 end
